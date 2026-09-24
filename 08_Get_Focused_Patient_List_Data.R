90c82c4a4a0ca609615e10d68c0171a76d3cc9d3
James Routledge <james.routledge@health.org.uk>
2026-09-24 10:44:36 +0100

    Use high-level GP workforce FTE source

--- 08_Get_Focused_Patient_List_Data.R
+++ 08_Get_Focused_Patient_List_Data.R
@@ -190,89 +190,37 @@ practice_registered_snapshot <- DBI::dbGetQuery(
 ) %>%
   dplyr::mutate(Practice_Code = as.character(Practice_Code))
 
-workforce_table <- DBI::Id(
-  schema = "NHS_Workforce",
-  table = "Practice_Level_Census_Data1"
-)
-workforce_fields <- DBI::dbListFields(con, workforce_table)
-
-find_field <- function(fields, exact, regex) {
-  hit <- exact[exact %in% fields]
-  if (length(hit) > 0) return(hit[[1]])
-
-  hit <- fields[grepl(regex, fields, ignore.case = TRUE)]
-  if (length(hit) > 0) return(hit[[1]])
-
-  NA_character_
-}
-
-workforce_practice_field <- find_field(
-  workforce_fields,
-  exact = c(
-    "Practice_Code", "GP_Practice_Code", "PRACTICE_CODE",
-    "Practice code", "Organisation_Code", "Organisation code"
-  ),
-  regex = "practice.*code|organisation.*code|prac.*code"
-)
-
-workforce_date_field <- find_field(
-  workforce_fields,
-  exact = c(
-    "Effective_Snapshot_Date", "Snapshot_Date", "Snapshot date",
-    "Period", "Date"
-  ),
-  regex = "snapshot.*date|effective.*date|^period$|^date$"
-)
-
-workforce_fte_field <- find_field(
-  workforce_fields,
-  exact = c(
-    "TOTAL_GP_EXTG_FTE", "Total_GP_EXTG_FTE", "Qualified_GP_FTE",
-    "GP_FTE"
-  ),
-  regex = "total.*gp.*fte|qualified.*gp.*fte|(^|_)gp.*fte"
-)
-
-missing_workforce_fields <- c(
-  Practice_Code = workforce_practice_field,
-  Period = workforce_date_field,
-  GP_FTE = workforce_fte_field
-)
-missing_workforce_fields <- names(missing_workforce_fields)[
-  is.na(missing_workforce_fields)
-]
-
-if (length(missing_workforce_fields) > 0) {
-  stop(
-    "Could not identify the following workforce fields in ",
-    "NHS_Workforce.Practice_Level_Census_Data1: ",
-    paste(missing_workforce_fields, collapse = ", "),
-    ". Available fields are: ",
-    paste(workforce_fields, collapse = ", ")
-  )
-}
-
-q_practice <- as.character(DBI::dbQuoteIdentifier(con, workforce_practice_field))
-q_period <- as.character(DBI::dbQuoteIdentifier(con, workforce_date_field))
-q_fte <- as.character(DBI::dbQuoteIdentifier(con, workforce_fte_field))
-q_workforce_table <- as.character(DBI::dbQuoteIdentifier(con, workforce_table))
-
 qualified_gp_workforce <- DBI::dbGetQuery(
   con,
   glue::glue("
     SELECT
-      {q_practice} AS Practice_Code,
-      {q_period} AS Period,
-      SUM(TRY_CAST({q_fte} AS FLOAT)) AS GP_FTE
-    FROM {q_workforce_table}
-    WHERE {q_period} >= '{start_sql}'
-      AND {q_period} <= '{end_sql}'
-      AND {q_practice} IS NOT NULL
-    GROUP BY {q_practice}, {q_period}
-    ORDER BY {q_period}, {q_practice}
+      Practice_Name,
+      Practice_Code,
+      Effective_Snapshot_Date AS Period,
+      SUM(TRY_CAST(Measure_Value AS FLOAT)) AS GP_FTE
+    FROM NHS_Workforce.Practice_Level_Census_Data_High_Level1
+    WHERE Effective_Snapshot_Date >= '{start_sql}'
+      AND Effective_Snapshot_Date <= '{end_sql}'
+      AND Staff_Group = 'GP'
+      AND Measure = 'FTE'
+      AND Detailed_Staff_Role IN (
+        'Salaried By Practice',
+        'Salaried By Other',
+        'Partner/Provider',
+        'Senior Partner'
+      )
+      AND Practice_Code IS NOT NULL
+    GROUP BY
+      Practice_Name,
+      Practice_Code,
+      Effective_Snapshot_Date
+    ORDER BY
+      Effective_Snapshot_Date,
+      Practice_Code
   ")
 ) %>%
   dplyr::mutate(
+    Practice_Name = as.character(Practice_Name),
     Practice_Code = as.character(Practice_Code),
     Period = as.Date(Period),
     GP_FTE = as.numeric(GP_FTE)

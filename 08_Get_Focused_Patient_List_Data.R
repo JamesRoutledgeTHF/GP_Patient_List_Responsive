bf469774f00ae7f0e09aedbff207cf0de50f6d76
James Routledge <james.routledge@health.org.uk>
2026-09-24 13:37:42 +0100

    Use long-run registered patient source for England totals

--- 08_Get_Focused_Patient_List_Data.R
+++ 08_Get_Focused_Patient_List_Data.R
@@ -41,10 +41,10 @@ national_registered <- DBI::dbGetQuery(
     SELECT
       Effective_Snapshot_Date AS Period,
       SUM(Size) AS Population
-    FROM Demography.No_Of_Patients_Regd_At_GP_Practice_LSOA_2021_Level1
+    FROM Demography.No_Of_Patients_Regd_At_GP_Practice_Single_Age1
     WHERE Effective_Snapshot_Date >= '{start_sql}'
       AND Effective_Snapshot_Date <= '{end_sql}'
-      AND LSOA_Code LIKE 'E01%'
+      AND UPPER(Sex) IN ('FEMALE', 'MALE')
     GROUP BY Effective_Snapshot_Date
     ORDER BY Effective_Snapshot_Date
   ")
